7cba30dcf79319d7cd98aa3b2ce3e37142730da3
Yitong Sun <syitong@umich.edu>
2017-03-30 12:35:41 -0400

    delete all matlab code from the pycode branch.

--- matlab/RFF.m
+++
@@ -1,5 +0,0 @@
-% this is used to convert dataset X to Xtil via Random Fourier Features
-
-function Xtil = RRF(X,NoFeatures,sigma)
-W = randn(NoFeatures,size(X,2));
-Xtil = [cos(X*W'/sigma),sin(X*W'/sigma)]/sqrt(NoFeatures);
