f47ccc976e62121bfd6eeebbc854fdfb745f1a64
Yitong Sun <syitong@umich.edu>
2017-04-01 23:52:58 -0400

    l1 and l2 now works correctly. custom random features method with cos and sin is added.

--- pycode/main.py
+++ pycode/main.py
@@ -8,28 +8,31 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 # my module
 import datagen, dataplot
+import rff
 
 ### set up parameters
 datarange = 0.5
 overlap = 0
-samplesize = 500
-logclist = np.arange(-3,4,0.5)
+samplesize = 1500
+logclist = np.arange(-3,4.5,0.5)
 trials = 1
 
 ### generate train and test dataset
 X,Y = datagen.unit_circle(datarange,overlap,samplesize)
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size = 0.4,random_state=0)
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.33,random_state=0)
 
 ### estimate gamma in the rbf kernel
 gamma = datagen.gamma_est(X_train)
 
 ### rbf kernel support vector machine
 kscore = list()
+ksparsity = list()
 for idx in range(len(logclist)):
     C = 10**logclist[idx]
     clf = svm.SVC(C=C,gamma=gamma)
     clf.fit(X_train,Y_train)
     kscore.append(clf.score(X_test,Y_test))
+    ksparsity.append(clf.n_support_)
 
 ### full and sparse random features method
 l1score = list()
@@ -37,27 +40,30 @@ l2score = list()
 l1sparsity = list()
 l2sparsity = list()
 for idx in range(trials):
-    rbf_feature = RBFSampler(gamma=gamma,n_components=40)
-    scaler = StandardScaler()
+    #rbf_feature = rff.myRBFSampler(gamma=gamma,n_old_features=X_train.shape[1])
+    rbf_feature = RBFSampler(gamma=gamma,n_components=20)
     X_train_til = rbf_feature.fit_transform(X_train)
-    scaler.fit(X_train_til)
-    X_train_til = scaler.transform(X_train_til)
-    X_test_til = rbf_feature.fit_transform(X_test)
-    X_test_til = scaler.transform(X_test_til)
+    X_test_til = rbf_feature.transform(X_test)
     m = X_train_til.shape[0]
     for idx in range(len(logclist)):
         C = 10**logclist[idx]
         clfl1 = SGDClassifier(loss='hinge',penalty='l1',alpha=1/C/m)
+        #clfl1 = svm.LinearSVC(penalty='l1',C=C,dual=False)
         clfl1.fit(X_train_til,Y_train)
         l1score.append(clfl1.score(X_test_til,Y_test))
         l1sparsity.append(np.sum(clfl1.coef_!=0))
         clfl2 = SGDClassifier(loss='hinge',penalty='l2',alpha=1/C/m)
+        #clfl2 = svm.LinearSVC(loss='hinge',penalty='l2',C=C)
         clfl2.fit(X_train_til,Y_train)
         l2score.append(clfl2.score(X_test_til,Y_test))
         l2sparsity.append(np.sum(clfl2.coef_!=0))
+    l1score = np.array(l1score)
+    l2score = np.array(l2score)
+    np.set_printoptions(precision=2)
+    print l1score,'\n',l2score
 
-plt.plot(logclist,kscore,'b-o',fillstyle='none')
-plt.plot(logclist,l2score,'r--s',fillstyle='none')
+plt.plot(logclist,kscore,'r-o',fillstyle='none')
+plt.plot(logclist,l2score,'b--s',fillstyle='none')
 plt.plot(logclist,l1score,'g:x',fillstyle='none')
 plt.xlabel('$\log(C)$')
 plt.ylabel('accuracy')
@@ -68,3 +74,6 @@ with open('result/l1spasity.csv','wb') as csvfile:
 with open('result/l2spasity.csv','wb') as csvfile:
     datawriter = csv.writer(csvfile,delimiter=' ')
     datawriter.writerow(l2sparsity)
+with open('result/kspasity.csv','wb') as csvfile:
+    datawriter = csv.writer(csvfile,delimiter=' ')
+    datawriter.writerow(ksparsity)
---
+++ pycode/rff.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class myRBFSampler:
+    def __init__(self,n_old_features,gamma=1,n_components=20):
+        self.sampler = np.random.randn(n_components,n_old_features)*np.sqrt(gamma)
+        self.gamma = gamma
+        self.n_components = n_components
+
+    def fit_transform(self, X):
+        n = X.shape[0]
+        X_til = list()
+        c = np.cos(np.dot(X, self.sampler))
+        s = np.sin(np.dot(X, self.sampler))
+        X_til = np.append(c, s, 1)
+        return X_til / np.sqrt(self.n_components)
