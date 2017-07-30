a5f8be3a5ee13c1c0151b713b8de6ef34f5bddf5
Yitong Sun <syitong@umich.edu>
2017-07-30 00:54:44 -0400

    HyperRFSVM is written, but not tested.

--- pycode/rff.py
+++ pycode/rff.py
@@ -10,7 +10,7 @@ class myRBFSampler:
         self.gamma = gamma
         self.n_components = n_components
 
-    def fit_transform(self, X, mask=0):
+    def fit_transform(self, X, mask):
         """
         mask is a list of the same length with the feature
         vector but with only 1s at very sparse positions.
@@ -29,3 +29,37 @@ class myRBFSampler:
                 X_sin = np.concatenate((X_sin,np.sin(X.dot(self.sampler[:,idx])).reshape(n,1)),1)
         X_til = np.concatenate((X_cos,X_sin),1)
         return X_til / np.sqrt(self.n_components)
+
+class HyperRFSVM:
+    """
+    This class implements the RFSVM with random drop out for each round
+    of subgrad descent.
+    """
+    def __init__(self,sampler,eta=1,p=0):
+        self.eta = eta
+        self.sampler = sampler
+        self.p = p
+        self.w = np.zeros(sampler.n_components)
+
+    def train(self,cycle,X,Y):
+        """
+        We run the cyclic subgradient descent. cycle is the number of
+        repeats of the cycles of the dataset.
+        """
+        for idx in range(cycle):
+            n = len(Y)
+            for jdx in range(n):
+                self.partial_train(X[jdx,:],Y[jdx],jdx+idx*n)
+        return 1
+
+    def partial_train(self,Xrow,y,T):
+        mask = np.random.binomial(1,self.p,self.sampler.n_components)
+        Xrow_til = self.sampler.fit_transform(Xrow,mask)
+        if np.dot(Xrow_til,self.w)*y < 1:
+            self.w = self.w + y*Xrow*self.eta / T
+        return 1
+
+    def test(self,X):
+        mask = np.ones(shape(X)[1])
+        X_til = self.sampler.fit_transform(X,mask)
+        return np.sign(X_til.dot(self.w.T))
--- pycode/test.py
+++ pycode/test.py
@@ -3,8 +3,4 @@ import matplotlib.pyplot as plt
 import datagen, dataplot
 import rff
 
-Sampler = rff.myRBFSampler(n_old_features=3,n_components=5)
-X = np.array([[1,2,3]])
-mask = [0,1,0,0,1]
-X_til = Sampler.fit_transform(X,mask)
-print X_til
+
