74330238ebdf4cd4899961906b65683a2c753213
Sun, Yitong <sunny19890224@gmail.com>
2017-12-12 02:22:57 -0500

    tfRF2L is done. Performance needs to be checked.

--- pycode/test.py
+++ pycode/test.py
@@ -4,10 +4,21 @@ import rff
 import tensorflow as tf
 import tfRF2L
 
-n_old_features = 10
+X,Y = rff.unit_circle_ideal(0.1,0.9,300)
+Xtrain = X[:200]
+Ytrain = Y[:200]
+Xtest = X[200:]
+Ytest = Y[200:]
+n_old_features = 2
 n_components = 20
-Lambda = 1
-Gamma = 1
-n_classes = 5
+Lambda = 0.001
+Gamma = rff.gamma_est(Xtrain)
+n_classes = 2
 clf = tfRF2L.tfRF2L(n_old_features,n_components,
     Lambda,Gamma,n_classes)
+clf.fit(Xtrain,Ytrain,'layer 2',1000)
+Ypred = clf.predict(Xtest)['classes']
+print(Ypred)
+print(Ytest)
+accuracy = np.sum(Ytest==Ypred) / 100
+print('accuracy={0:.3f}'.format(accuracy))
--- pycode/tfRF2L.py
+++ pycode/tfRF2L.py
@@ -21,9 +21,7 @@ class tfRF2L:
         self._loss_fn = loss_fn
         self._sess = tf.Session()
         self._model_fn()
-        self._global_step_layer_2 = tf.Variable(0,trainable=False)
-        self._global_step_layer_1 = tf.Variable(0,trainable=False)
-        self._summary = None
+        self._sess.run(tf.global_variables_initializer())
 
     @property
     def d(self):
@@ -60,13 +58,14 @@ class tfRF2L:
 
         with self._sess.graph.as_default():
             g = self._sess.graph
-            with tf.name_scope('inputs'):
-                x = tf.placeholder(dtype=tf.float32,
-                    shape=[None,d],name='features')
-                y = tf.placeholder(dtype=tf.uint8,
-                    shape=[None,1],name='labels')
+            global_step_1 = tf.Variable(0,trainable=False,name='global1')
+            global_step_2 = tf.Variable(0,trainable=False,name='global2')
+            x = tf.placeholder(dtype=tf.float32,
+                shape=[None,d],name='features')
+            y = tf.placeholder(dtype=tf.uint8,
+                shape=[None],name='labels')
 
-            with tf.name_scope('Layer1'):
+            with tf.name_scope('RF_layer'):
                 initializer = tf.random_normal_initializer(
                     stddev=tf.sqrt(Gamma))
 
@@ -74,113 +73,121 @@ class tfRF2L:
                     use_bias=False,
                     kernel_initializer=initializer,
                     name='Gaussian')
+                self._sess.run(tf.global_variables_initializer())
 
                 cos_layer = tf.cos(trans_layer)
                 sin_layer = tf.sin(trans_layer)
-                RF_layer = tf.div([cos_layer,sin_layer],
-                    tf.sqrt(N*1.0))
+                concated = tf.concat([cos_layer,sin_layer],axis=1)
+                RF_layer = tf.div(concated,tf.sqrt(N*1.0))
                 tf.summary.histogram('inner weights',
                     g.get_tensor_by_name('Gaussian/kernel:0'))
 
-            with tf.name_scope('Layer2'):
-                logits = tf.layers.dense(inputs=RF_layer,
-                    kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=Lambda),
-                    units=n_classes,name='logits')
-                tf.summary.histogram('outer weights',
-                    g.get_tensor_by_name('logits/kernel:0'))
+            logits = tf.layers.dense(inputs=RF_layer,
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=Lambda),
+                units=n_classes,name='Logits')
+            tf.add_to_collection("Probab",logits)
+            tf.summary.histogram('outer weights',
+                g.get_tensor_by_name('Logits/kernel:0'))
 
             probab = tf.nn.softmax(logits, name="softmax")
-            with tf.name_scope('Loss'):
-                # hinge loss only works for binary classification.
-                regularizer = tf.losses.get_regularization_loss(
-                    scope = 'Layer2'
-                )
-                if self._n_classes == 2:
-                    loss_hinge = tf.losses.hinge_loss(labels=y,
-                        logits=logits,
-                        loss_collection="loss") + regularizer
-                    loss_ramp = (tf.max(loss_hinge,1,name="ramp loss")
-                        + regularizer)
-                    tf.add_to_collection("loss",loss_ramp)
-                onehot_labels = tf.one_hot(indices=tf.cast(y, tf.uint8),
-                    depth=n_classes)
-                loss_log = tf.losses.softmax_cross_entropy(
-                    onehot_labels=onehot_labels, logits=logits,
-                    loss_collection="loss") + regularizer
+            tf.add_to_collection("Probab",probab)
+
+            # hinge loss only works for binary classification.
+            regularizer = tf.losses.get_regularization_loss(scope='Logits')
+            # if self._n_classes == 2:
+            #     loss_hinge = tf.losses.hinge_loss(labels=y,
+            #         logits=logits,
+            #         loss_collection="loss") + regularizer
+            #     loss_ramp = (tf.max(loss_hinge,1)
+            #         + regularizer)
+            #     tf.add_to_collection("loss",loss_ramp)
+            onehot_labels = tf.one_hot(indices=tf.cast(y, tf.uint8),
+                depth=n_classes)
+            loss_log = tf.losses.softmax_cross_entropy(
+                onehot_labels=onehot_labels, logits=logits)
+            reg_log_loss = tf.add(loss_log,regularizer)
+            tf.add_to_collection('Loss',reg_log_loss)
+
             merged = tf.summary.merge_all()
-            self._train_writer = tf.summary.FileWriter('log',
+            tf.add_to_collection('Summary',merged)
+            self._train_writer = tf.summary.FileWriter('tmp',
                 tf.get_default_graph())
             self._sess.run(tf.global_variables_initializer())
             summary = self._sess.run(merged)
             self._train_writer.add_summary(summary)
 
     def predict(self,data):
-        with self._graph.as_default():
-            x = tf.get_tensor_by_name('features:0')
-            feed_dict = {x:data}
-            logits = tf.get_tensor_by_name('logits:0')
-            probab = tf.get_tensor_by_name('softmax')
+        with self._sess.graph.as_default():
+            g = self._sess.graph
+            feed_dict = {'features:0':data}
+            logits,probab = tf.get_collection('Probab')
             predictions = {
                 # Generate predictions (for PREDICT and EVAL mode)
-                "classes": tf.argmax(input=logits, axis=1),
+                "classes": tf.argmax(input=logits,axis=1),
                 # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
                 # `logging_hook`.
                 "probabilities": probab}
-            return self._sess(predictions,feed_dict=feed_dict)
+            return self._sess.run(predictions,feed_dict=feed_dict)
 
     def fit(self,data,labels,mode,n_iter):
-        with self._graph.as_default():
-            out_weights = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                'Layer2')
+        with self._sess.graph.as_default():
+            g = self._sess.graph
             in_weights = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
-                'Layer1')
-            if self._n_classes == 2:
-                h_loss,r_loss,l_loss = tf.get_collection('loss')
-            else:
-                l_loss = tf.get_collection('loss')
-            if self._loss_fn == 'hinge loss':
-                loss = h_loss
-            elif self._loss_fn == 'log loss':
-                loss = l_loss
+                'RF_layer')
+            out_weights = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                'Logits')
+            # if self._n_classes == 2:
+            #     h_loss,r_loss,l_loss = tf.get_collection('loss')
+            # else:
+            #     l_loss = tf.get_collection('loss')
+            # if self._loss_fn == 'hinge loss':
+            #     loss = h_loss
+            # elif self._loss_fn == 'log loss':
+            #     loss = l_loss
+            loss = tf.get_collection('Loss')[0]
+            global_step_1 = g.get_tensor_by_name('global1:0')
+            global_step_2 = g.get_tensor_by_name('global2:0')
+            merged = tf.get_collection('Summary')[0]
             if mode == 'layer 2':
                 learning_rate = tf.train.inverse_time_decay(
                     learning_rate=1.,
                     decay_steps=1,
-                    global_step=self._global_step_layer_2,
+                    global_step=global_step_2,
                     decay_rate=1.)
                 optimizer = tf.train.GradientDescentOptimizer(learning_rate)
                 # optimizer = tf.train.FtrlOptimizer(learning_rate=50,
                  #   l2_regularization_strength=0.)
                 train_op = optimizer.minimize(
                     loss=loss,
-                    global_step=self._global_step_layer_2,
+                    global_step=global_step_2,
                     var_list=out_weights
                 )
             if mode == 'layer 1':
                 learning_rate = tf.train.inverse_time_decay(
                     learning_rate=1.,
                     decay_steps=1,
-                    global_step=self._global_step_layer_1,
+                    global_step=global_step_1,
                     decay_rate=1.)
                 optimizer = tf.train.GradientDescentOptimizer(learning_rate)
                 # optimizer = tf.train.FtrlOptimizer(learning_rate=50,
                  #   l2_regularization_strength=0.)
                 train_op = optimizer.minimize(
                     loss=loss,
-                    global_step=self._global_step_layer_1,
+                    global_step=global_step_1,
                     var_list=in_weights
                 )
             for idx in range(n_iter):
-                feed_dict = {x:data[idx],
-                             y:label[idx]}
+                rand_i = np.random.randint(len(data))
+                feed_dict = {'features:0':data[None,rand_i,:],
+                             'labels:0':labels[None,rand_i]}
                 if idx % 10 == 1:
-                    print('loss: {.4f}'.format(self._sess.run(loss,feed_dict)))
-                    summary = self._sess.run(self._summary)
-                    self._train_writer.add_summary(summary,i)
+                    print('loss: {0:.4f}'.format(self._sess.run(loss,feed_dict)))
+                    summary = self._sess.run(merged)
+                    self._train_writer.add_summary(summary,idx)
                 self._sess.run(train_op,feed_dict)
-            print('loss: {.4f}'.format(self._sess.run(loss,feed_dict)))
-            summary = self._sess.run(self._summary)
-            self._train_writer.add_summary(summary,i)
+            print('loss: {0:.4f}'.format(self._sess.run(loss,feed_dict)))
+            summary = self._sess.run(merged)
+            self._train_writer.add_summary(summary,idx)
 
     def get_params(self):
         params = {
