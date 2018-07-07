ab53b9b0a326de17aea651a79837fc8d45502a42
Sun, Yitong <sunny19890224@gmail.com>
2018-07-06 22:24:17 -0400

    plot trig vs relu learning rate
    ResultPlot.py is modified to draw the graph.
    Previously it's for opt vs unif.

--- pycode/ResultPlot.py
+++ pycode/ResultPlot.py
@@ -1,25 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import rff
-import tensorflow as tf
 
 samplesize = np.arange(1000,60001,5000)
 orfsvm = np.zeros((10,len(samplesize)))
 urfsvm = np.zeros((10,len(samplesize)))
-for idx in range(1,11,1):
-    orfsvm[idx-1,:] = np.loadtxt('result/ORFSVM'+str(idx))
-    urfsvm[idx-1,:] = np.loadtxt('result/URFSVM'+str(idx))
+for prefix in range(1,11,1):
+    orfsvm[prefix-1,:] = np.loadtxt('result/tfRFSVM_{0:s}{1:s}'.format('Gaussian',
+        str(prefix)))
+    urfsvm[prefix-1,:] = np.loadtxt('result/tfRFSVM_{0:s}{1:s}'.format('ReLU',
+        str(prefix)))
 
 orfmean = np.mean(orfsvm,axis=0)
 urfmean = np.mean(urfsvm,axis=0)
 orfstd = np.std(orfsvm,axis=0)
 urfstd = np.std(urfsvm,axis=0)
 
-plt.title("opt vs unif feature selection on MNIST")
+# opt vs unif feature selection
+# plt.title("opt vs unif feature selection on MNIST")
+# plt.xlabel('sample size (k)')
+# plt.ylabel('accuracy')
+# plt.xticks(samplesize/1000)
+# plt.errorbar(samplesize/1000,orfmean,yerr=orfstd,fmt='bs--',label='opt',fillstyle='none')
+# plt.errorbar(samplesize/1000,urfmean,yerr=urfstd,fmt='gx:',label='unif')
+# plt.legend(loc=4)
+# plt.savefig('image/opt_vs_unif.eps')
+
+# Gaussian vs ReLU random features
+plt.title("Trig vs ReLU features on MNIST")
 plt.xlabel('sample size (k)')
 plt.ylabel('accuracy')
 plt.xticks(samplesize/1000)
-plt.errorbar(samplesize/1000,orfmean,yerr=orfstd,fmt='bs--',label='opt',fillstyle='none')
-plt.errorbar(samplesize/1000,urfmean,yerr=urfstd,fmt='gx:',label='unif')
+plt.errorbar(samplesize/1000,orfmean,yerr=orfstd,fmt='bs--',label='Trig',fillstyle='none')
+plt.errorbar(samplesize/1000,urfmean,yerr=urfstd,fmt='gx:',label='ReLU')
 plt.legend(loc=4)
-plt.savefig('image/opt_vs_unif.eps')
+plt.savefig('image/Trig_vs_ReLU.eps')
