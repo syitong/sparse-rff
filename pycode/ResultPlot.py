463e9764eb5d9c1b96a1bffb17b38401d3a1e499
Sun, Yitong <sunny19890224@gmail.com>
2018-07-07 00:45:06 -0400

    plot over all mode result
    ResultPlot.py is modified to draw the graph.

--- pycode/ResultPlot.py
+++ pycode/ResultPlot.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 samplesize = np.arange(1000,60001,5000)
 orfsvm = np.zeros((10,len(samplesize)))
 urfsvm = np.zeros((10,len(samplesize)))
+mode = 'over all'
 for prefix in range(1,11,1):
-    orfsvm[prefix-1,:] = np.loadtxt('result/tfRFSVM_{0:s}{1:s}'.format('Gaussian',
-        str(prefix)))
-    urfsvm[prefix-1,:] = np.loadtxt('result/tfRFSVM_{0:s}{1:s}'.format('ReLU',
-        str(prefix)))
+    orfsvm[prefix-1,:] = np.loadtxt('result/tfRF2L_{0:s}{2:s}{1:s}'.format('Gaussian',str(prefix),mode))
+    urfsvm[prefix-1,:] = np.loadtxt('result/tfRF2L_{0:s}{2:s}{1:s}'.format('ReLU',str(prefix),mode))
 
 orfmean = np.mean(orfsvm,axis=0)
 urfmean = np.mean(urfsvm,axis=0)
@@ -26,11 +25,11 @@ urfstd = np.std(urfsvm,axis=0)
 # plt.savefig('image/opt_vs_unif.eps')
 
 # Gaussian vs ReLU random features
-plt.title("Trig vs ReLU features on MNIST")
+plt.title("Trig vs ReLU NN Nodes on MNIST")
 plt.xlabel('sample size (k)')
 plt.ylabel('accuracy')
 plt.xticks(samplesize/1000)
 plt.errorbar(samplesize/1000,orfmean,yerr=orfstd,fmt='bs--',label='Trig',fillstyle='none')
 plt.errorbar(samplesize/1000,urfmean,yerr=urfstd,fmt='gx:',label='ReLU')
 plt.legend(loc=4)
-plt.savefig('image/Trig_vs_ReLU.eps')
+plt.savefig('image/Trig_vs_ReLU_NN.eps')
