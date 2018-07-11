2cca9e93a5a42036060031837d627993ea390ee2
Sun, Yitong <sunny19890224@gmail.com>
2018-07-11 00:40:00 -0400

    plot layer 1 vs over all
    Layer 1 mode is marginally worse than overall mode.

--- pycode/ResultPlot.py
+++ pycode/ResultPlot.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 samplesize = np.arange(1000,60001,5000)
 orfsvm = np.zeros((10,len(samplesize)))
 urfsvm = np.zeros((10,len(samplesize)))
-mode = 'over all'
+# mode = 'over all'
 for prefix in range(1,11,1):
-    orfsvm[prefix-1,:] = np.loadtxt('result/tfRF2L_{0:s}{2:s}{1:s}'.format('Gaussian',str(prefix),mode))
-    urfsvm[prefix-1,:] = np.loadtxt('result/tfRF2L_{0:s}{2:s}{1:s}'.format('ReLU',str(prefix),mode))
+    orfsvm[prefix-1,:] = np.loadtxt('result/tfRF2L_{0:s}{2:s}{1:s}'.format('ReLU',str(prefix),'over all'))
+    urfsvm[prefix-1,:] = np.loadtxt('result/tfRF2L_{0:s}{2:s}{1:s}'.format('ReLU',str(prefix),'layer 1'))
 
 orfmean = np.mean(orfsvm,axis=0)
 urfmean = np.mean(urfsvm,axis=0)
@@ -25,11 +25,11 @@ urfstd = np.std(urfsvm,axis=0)
 # plt.savefig('image/opt_vs_unif.eps')
 
 # Gaussian vs ReLU random features
-plt.title("Trig vs ReLU NN Nodes on MNIST")
+plt.title("Overall vs Layer1 ReLU on MNIST")
 plt.xlabel('sample size (k)')
 plt.ylabel('accuracy')
 plt.xticks(samplesize/1000)
-plt.errorbar(samplesize/1000,orfmean,yerr=orfstd,fmt='bs--',label='Trig',fillstyle='none')
-plt.errorbar(samplesize/1000,urfmean,yerr=urfstd,fmt='gx:',label='ReLU')
+plt.errorbar(samplesize/1000,orfmean,yerr=orfstd,fmt='bs--',label='over all',fillstyle='none')
+plt.errorbar(samplesize/1000,urfmean,yerr=urfstd,fmt='gx:',label='layer 1')
 plt.legend(loc=4)
-plt.savefig('image/Trig_vs_ReLU_NN.eps')
+plt.savefig('image/Overall_vs_Layer1_ReLU.eps')
