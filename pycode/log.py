bdeba5ecefbd749ad0ab8fde86048ef2fa89a557
Sun, Yitong <sunny19890224@gmail.com>
2018-07-14 22:55:20 -0400

    add train and test time into result list
    Turn off the screen output.

--- pycode/log.py
+++ pycode/log.py
@@ -6,27 +6,31 @@ class log:
     and write it into a log file.
     """
     def __init__(self,filepath,init_message):
-        self.message = ''
-        self.filepath = filepath
-        self.progress = {'task':[init_message],'time':[time.process_time()]}
-        print(self.progress['task'][-1]
-              + ': {:.4f}'.format(self.progress['time'][-1]))
+        self._message = ''
+        self._filepath = filepath
+        self._progress = {'task':[init_message],'time':[time.process_time()]}
+        print(self._progress['task'][-1]
+              + ': {:.4f}'.format(self._progress['time'][-1]))
+
+    @property
+    def progress(self):
+        return self._progress
 
     def time_event(self,message):
-        self.progress['task'].append(message)
-        self.progress['time'].append(time.process_time())
-        print(self.progress['task'][-1]
-              + ': {:.4f}'.format(self.progress['time'][-1]
-                              - self.progress['time'][-2]))
+        self._progress['task'].append(message)
+        self._progress['time'].append(time.process_time())
+        print(self._progress['task'][-1]
+              + ': {:.4f}'.format(self._progress['time'][-1]
+                              - self._progress['time'][-2]))
 
     def record(self,message):
-        self.message = message
+        self._message = message
 
     def save(self):
-        progress = self.progress
-        with open(self.filepath,'w') as logfile:
+        progress = self._progress
+        with open(self._filepath,'w') as logfile:
             for idx in range(1,len(progress['task'])):
                 logfile.write(progress['task'][idx]
                               + ': {:.4f}\n'.format(progress['time'][idx]
                                                     - progress['time'][idx - 1]))
-            logfile.write(self.message)
+            logfile.write(self._message)
--- pycode/mnist_nn.py
+++ pycode/mnist_nn.py
@@ -12,23 +12,15 @@ from libmnist import plot_confusion_matrix, get_train_test_data
 
 def tfRF2L_MNIST(m=1000,n_components=1000,feature='ReLU',mode='layer 2'):
     # set up timer and progress tracker
-    mylog = log.log('log/tfRF2L_{2:s}{3:s}_m_{0:.2e}_N_{1:.2e}.log'.format(m,
-        n_components,feature,mode),
-        'MNIST classification starts')
+    mylog = log.log('log/tmp.log','MNIST classification starts')
     Xtrain,Ytrain,Xtest,Ytest = get_train_test_data(train_size=m,
         test_size=int(m/3))
-    mylog.time_event('data read in complete')
 
     # set up parameters
-    LogLambda = np.arange(-12.0,-2,1.)
-    # gamma = rff.gamma_est(Xtrain)
-    # LogGamma = np.arange(-0.2,0.8,.1)
-    # LogGamma = np.log10(gamma) + LogGamma
     params = {
         'feature': feature,
         'n_old_features': len(Xtrain[0]),
         'n_components': n_components,
-        # 'Lambda': np.float32(10.**(-6)),
         'Lambda': np.float32(0.),
         'Gamma': np.float32(10.**(-3)),
         'classes': [0,1,2,3,4,5,6,7,8,9],
@@ -41,81 +33,46 @@ def tfRF2L_MNIST(m=1000,n_components=1000,feature='ReLU',mode='layer 2'):
         'n_iter': m
     }
 
-    # hyper-parameter selection
-    best_score = 0
-    best_Gamma = 1
-    # best_Lambda = 10.**(-6)
-    best_Lambda = 0.
-    crossval_result = {'Gamma':[],'Lambda':[],'score':[]}
-    # for idx in range(len(LogGamma)):
-    #     Gamma = np.float32(10**LogGamma[idx])
-    #     for jdx in range(len(LogLambda)):
-    #         Lambda = np.float32(10**LogLambda[jdx])
-    #         params['Lambda'] = Lambda
-    #         params['Gamma'] = Gamma
-    #         clf = rff.tfRF2L(**params)
-    #         score = cross_val_score(clf,Xtrain,Ytrain,fit_params=fit_params,cv=5)
-    #         mylog.time_event('Gamma={0:.1e} and Lambda={1:.1e}\n'.format(Gamma,Lambda)
-    #                          +'crossval done')
-    #         crossval_result['Gamma'].append(Gamma)
-    #         crossval_result['Lambda'].append(Lambda)
-    #         avg_score = np.sum(score) / 5
-    #         print('score = {:.4f}'.format(avg_score))
-    #         crossval_result['score'].append(avg_score)
-    #         if avg_score > best_score:
-    #             best_score = avg_score
-    #             best_Gamma = Gamma
-    #             best_Lambda = Lambda
-    #             best_clf = clf
-
     # performance test
     best_clf = rff.tfRF2L(**params)
-    best_clf.log = True
+    best_clf.log = False
     best_clf.fit(Xtrain,Ytrain,**fit_params)
     mylog.time_event('best model trained')
+    train_time = mylog.progress['time'][-1] - mylog.progress['time'][-2]
     Ypred,_,sparsity = best_clf.predict(Xtest)
     C_matrix = confusion_matrix(Ytest,Ypred)
     score = np.sum(Ypred == Ytest) / len(Ytest)
     mylog.time_event('test done')
-
-    # write results and log files
-    classes = range(10)
-    results = ('Best Gamma = {:.1e}\n'.format(best_Gamma)
-               + 'Best Lambda = {:.1e}\n'.format(best_Lambda)
-               + 'Classification Accuracy = {}\n'.format(score))
-    print(results)
-    results = results + 'Gamma    Lambda    score\n'
-    for idx in range(len(crossval_result['Gamma'])):
-        results = (results
-                   + '{0:.1e}{1:9.1e}{2:10.4f}\n'.format(crossval_result['Gamma'][idx],
-                                                         crossval_result['Lambda'][idx],
-                                                         crossval_result['score'][idx]))
-    mylog.record(results)
-    mylog.save()
+    test_time = mylog.progress['time'][-1] - mylog.progress['time'][-2]
 
     # plot confusion matrix
     fig = plt.figure()
-    plot_confusion_matrix(C_matrix,classes=classes,normalize=True)
+    plot_confusion_matrix(C_matrix,classes=range(10),normalize=True)
     plt.savefig('image/tfRF2L_{2:s}{3:s}_m_{0:.2e}_N_{1:.2e}.eps'.format(m,
         n_components,feature,mode))
     plt.close(fig)
 
-    return [score,sparsity]
+    print('''
+    score:{0:.4f}
+    sparsity:{1:.4f}
+    train_time:{2:.4f}
+    test_time:{3:.4f}'''.format(score,sparsity,train_time,test_time))
+    return [score,sparsity,train_time,test_time]
 
 def main():
     prefix = argv[1]
     score_list = []
     feature = 'ReLU'
-    mode = 'over all'
-    increment = 5000
-    for m in range(1000,60001,increment):
+    mode = 'layer 2'
+    increment = 500000
+    for m in range(5000,60001,increment):
         score = tfRF2L_MNIST(m=m,n_components=int(np.sqrt(m)),feature=feature,mode=mode)
         score_list.append(score)
     np.savetxt('result/tfRF2L_{0:s}{2:s}{1:s}'.format(feature,str(prefix),mode),np.array(score_list))
     score_list = []
-    mode = 'over all'
+    mode = 'layer 2'
     feature = 'Gaussian'
-    for m in range(1000,60001,increment):
+    for m in range(5000,60001,increment):
         score = tfRF2L_MNIST(m=m,n_components=int(np.sqrt(m)),feature=feature,mode=mode)
         score_list.append(score)
     np.savetxt('result/tfRF2L_{0:s}{2:s}{1:s}'.format(feature,str(prefix),mode),np.array(score_list))
