9a5418facdad009fb34bad015f4c2a432ed40e0f
Yitong Sun <syitong@umich.edu>
2018-08-17 10:06:56 -0400

    test covtype using best opt rate

--- pycode/covtype_nn.py
+++ pycode/covtype_nn.py
@@ -15,10 +15,10 @@ def covtype_nn(m=1000,n_components=1000,feature='ReLU',
     # set up timer and progress tracker
     mylog = log.log('log/tmp.log','Covtype classification starts')
     rand_list = np.random.permutation(Xtrain.shape[0])
-    Xtr = Xtrain[rand_list[:int(m*0.9)]]
-    Ytr = Ytrain[rand_list[:int(m*0.9)]]
-    Xval = Xtrain[rand_list[int(m*0.9):]]
-    Yval = Ytrain[rand_list[int(m*0.9):]]
+    Xtr = Xtrain[rand_list[:int(m*1.)]]
+    Ytr = Ytrain[rand_list[:int(m*1.)]]
+    Xval = Xtrain[rand_list[int(m*1.):]]
+    Yval = Ytrain[rand_list[int(m*1.):]]
     # set up parameters
     params = {
         'feature': feature,
@@ -45,12 +45,12 @@ def covtype_nn(m=1000,n_components=1000,feature='ReLU',
     best_clf.fit(Xtr,Ytr,**fit_params)
     mylog.time_event('best model trained')
     train_time = mylog.progress['time'][-1] - mylog.progress['time'][-2]
-    Ypred,_,sparsity = best_clf.predict(Xval,50)
-    # Ypred,_,sparsity = best_clf.predict(Xtest)
+    # Ypred,_,sparsity = best_clf.predict(Xval,50)
+    Ypred,_,sparsity = best_clf.predict(Xtest,50)
     mylog.time_event('test done')
     test_time = mylog.progress['time'][-1] - mylog.progress['time'][-2]
-    score = np.sum(Ypred == Yval) / len(Yval)
-    # score = np.sum(Ypred == Ytest) / len(Ytest)
+    # score = np.sum(Ypred == Yval) / len(Yval)
+    score = np.sum(Ypred == Ytest) / len(Ytest)
 
     print('''
     score:{0:.4f}
@@ -61,44 +61,44 @@ def covtype_nn(m=1000,n_components=1000,feature='ReLU',
 
 def main():
     prefix = argv[1]
-    m_max = 52291 # 1
+    m_max = 522911
     feature = 'ReLU'
     mode = 'layer 2'
     # run with best opt rate
-    # best_opt_rate = 10**2.0
-    # score = covtype_nn(m=m_max,n_components=500, #int(np.sqrt(m)),
-    #         feature=feature,mode=mode,opt_rate=best_opt_rate)
-    # np.savetxt('result/best_covtype_{0:s}{2:s}{1:s}'.format(feature,
-    #     str(prefix),mode),np.array(score))
+    best_opt_rate = 10**2.0
+    score = covtype_nn(m=m_max,n_components=5000, #int(np.sqrt(m)),
+            feature=feature,mode=mode,opt_rate=best_opt_rate)
+    np.savetxt('result/best_covtype_{0:s}{2:s}{1:s}'.format(feature,
+        str(prefix),mode),np.array(score))
 
     # select best opt rate
-    score_list = []
-    for log_opt_rate in np.arange(-2.,3.,0.5):
-        opt_rate = 10**log_opt_rate
-        score = covtype_nn(m=m_max,n_components=5000, #int(np.sqrt(m)),
-            feature=feature,mode=mode,opt_rate=opt_rate)
-        score_list.append(score)
-    np.savetxt('result/covtype_{0:s}{2:s}{1:s}'.format(feature,
-        str(prefix),mode),np.array(score_list))
+    # score_list = []
+    # for log_opt_rate in np.arange(-2.,3.,0.5):
+    #     opt_rate = 10**log_opt_rate
+    #     score = covtype_nn(m=m_max,n_components=5000, #int(np.sqrt(m)),
+    #         feature=feature,mode=mode,opt_rate=opt_rate)
+    #     score_list.append(score)
+    # np.savetxt('result/covtype_{0:s}{2:s}{1:s}'.format(feature,
+    #     str(prefix),mode),np.array(score_list))
 
     mode = 'layer 2'
     feature = 'Gaussian'
     # run with best opt rate
-    # best_opt_rate = 10**0.5
-    # score = covtype_nn(m=m_max,n_components=500, #int(np.sqrt(m)),
-    #     feature=feature,mode=mode,opt_rate=best_opt_rate)
-    # np.savetxt('result/best_covtype_{0:s}{2:s}{1:s}'.format(feature,
-    #     str(prefix),mode),np.array(score))
+    best_opt_rate = 10**1.0
+    score = covtype_nn(m=m_max,n_components=5000, #int(np.sqrt(m)),
+        feature=feature,mode=mode,opt_rate=best_opt_rate)
+    np.savetxt('result/best_covtype_{0:s}{2:s}{1:s}'.format(feature,
+        str(prefix),mode),np.array(score))
 
     # select best opt rate
-    score_list = []
-    for log_opt_rate in np.arange(-2.,3.,0.5):
-        opt_rate = 10**log_opt_rate
-        score = covtype_nn(m=m_max,n_components=5000, #int(np.sqrt(m)),
-            feature=feature,mode=mode,opt_rate=opt_rate)
-        score_list.append(score)
-    np.savetxt('result/covtype_{0:s}{2:s}{1:s}'.format(feature,
-        str(prefix),mode),np.array(score_list))
+    # score_list = []
+    # for log_opt_rate in np.arange(-2.,3.,0.5):
+    #     opt_rate = 10**log_opt_rate
+    #     score = covtype_nn(m=m_max,n_components=5000, #int(np.sqrt(m)),
+    #         feature=feature,mode=mode,opt_rate=opt_rate)
+    #     score_list.append(score)
+    # np.savetxt('result/covtype_{0:s}{2:s}{1:s}'.format(feature,
+    #     str(prefix),mode),np.array(score_list))
 
 if __name__ == '__main__':
     main()
