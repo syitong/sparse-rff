f77177b759013d01951e3e51a7af51f29e407f34
Sun, Yitong <sunny19890224@gmail.com>
2018-08-16 20:33:44 -0400

    get mnist summary

--- pycode/ResultPlot.py
+++ pycode/ResultPlot.py
@@ -13,10 +13,10 @@ for idx in range(4):
     urfsvm = np.zeros((10,len(x_labels)))
     for prefix in range(1,11,1):
         orfsvm[prefix-1,:] = np.loadtxt(
-            'result/tfRF2L_{0:s}{2:s}{1:s}'.format(
+            'result/adult_{0:s}{2:s}{1:s}'.format(
             'Gaussian',str(prefix), 'layer 2'))[:,idx]
         urfsvm[prefix-1,:] = np.loadtxt(
-            'result/tfRF2L_{0:s}{2:s}{1:s}'.format(
+            'result/adult_{0:s}{2:s}{1:s}'.format(
                 'ReLU',str(prefix),'layer 2'))[:,idx]
 
     orfmean = np.mean(orfsvm,axis=0)
@@ -36,12 +36,12 @@ for idx in range(4):
 
     # Gaussian vs ReLU random features
     fig = plt.figure()
-    plt.title("Fourier vs ReLU Feature {} on Mnist".format(tag))
+    plt.title("Fourier vs ReLU Feature {} on Adult".format(tag))
     plt.xlabel('log opt rate (-)')
     plt.ylabel('{0} ({1})'.format(tag,unit))
     plt.xticks(x_labels)
     plt.errorbar(x_labels,orfmean,yerr=orfstd,fmt='bs--',label='Fourier',fillstyle='none')
     plt.errorbar(x_labels,urfmean,yerr=urfstd,fmt='gx:',label='ReLU')
     plt.legend(loc=4)
-    plt.savefig('image/mnist_Fourier_vs_ReLU_{}.eps'.format(tag))
+    plt.savefig('image/adult_Fourier_vs_ReLU_{}.eps'.format(tag))
     plt.close(fig)
