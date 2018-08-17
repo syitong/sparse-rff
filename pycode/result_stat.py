2ef8139d6961c75618250397e09928551cc54097
Sun, Yitong <sunny19890224@gmail.com>
2018-08-16 21:15:33 -0400

    get adult summary

--- pycode/result_stat.py
+++ pycode/result_stat.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 tags = ['accuracy','sparsity','traintime','testtime']
-with open('result/best_mnist_summary','w') as f:
+with open('result/best_adult_summary','w') as f:
     for idx in range(4):
         tag = tags[idx]
         orfsvm = np.zeros(10)
         urfsvm = np.zeros(10)
         for prefix in range(1,11,1):
             orfsvm[prefix-1] = np.loadtxt(
-                'result/best_mnist_{0:s}{2:s}{1:s}'.format(
+                'result/best_adult_{0:s}{2:s}{1:s}'.format(
                 'Gaussian',str(prefix), 'layer 2'))[idx]
             urfsvm[prefix-1] = np.loadtxt(
-                'result/best_mnist_{0:s}{2:s}{1:s}'.format(
+                'result/best_adult_{0:s}{2:s}{1:s}'.format(
                     'ReLU',str(prefix),'layer 2'))[idx]
 
         orfmean = np.mean(orfsvm)
