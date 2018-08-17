4d0817f5282711e5b223faef2dcccbe9b6f3b133
Sun, Yitong <sunny19890224@gmail.com>
2018-08-16 22:59:27 -0400

    test opt rate for covtype

--- pycode/covtype_nn.py
+++ pycode/covtype_nn.py
@@ -44,7 +44,7 @@ def covtype_nn(m=1000,n_components=1000,feature='ReLU',
     best_clf.fit(Xtr,Ytr,**fit_params)
     mylog.time_event('best model trained')
     train_time = mylog.progress['time'][-1] - mylog.progress['time'][-2]
-    Ypred,_,sparsity = best_clf.predict(Xval)
+    Ypred,_,sparsity = best_clf.predict(Xval,50)
     # Ypred,_,sparsity = best_clf.predict(Xtest)
     mylog.time_event('test done')
     test_time = mylog.progress['time'][-1] - mylog.progress['time'][-2]
@@ -60,7 +60,7 @@ def covtype_nn(m=1000,n_components=1000,feature='ReLU',
 
 def main():
     prefix = argv[1]
-    m_max = 52291
+    m_max = 52291 # 1
     feature = 'ReLU'
     mode = 'layer 2'
     # run with best opt rate
@@ -93,7 +93,7 @@ def main():
     score_list = []
     for log_opt_rate in np.arange(-2.,3.,0.5):
         opt_rate = 10**log_opt_rate
-        score = covtype_nn(m=m_max,n_components=500, #int(np.sqrt(m)),
+        score = covtype_nn(m=m_max,n_components=5000, #int(np.sqrt(m)),
             feature=feature,mode=mode,opt_rate=opt_rate)
         score_list.append(score)
     np.savetxt('result/covtype_{0:s}{2:s}{1:s}'.format(feature,
