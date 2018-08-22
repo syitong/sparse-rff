e69252392fec08edfa138e9bd03364c46b43c615
Sun, Yitong <sunny19890224@gmail.com>
2018-08-21 21:29:10 -0400

    allocate covtype adam result

--- pycode/result_alloc.py
+++ pycode/result_alloc.py
@@ -3,10 +3,10 @@ import numpy as np
 def screen_params_alloc(dataset):
     filename = 'result/' + dataset + '-'
     row = ['log(rate)\log(Gamma)']
-    row.extend(np.arange(-6.,2,1))
+    row.extend(np.arange(-2.,4,0.5))
     row.append('ReLU')
     output = [row]
-    log_rate = np.arange(-2.,4,0.5)
+    log_rate = np.arange(-3.,3,0.5)
     for idx,rate in enumerate(log_rate):
         row = []
         with open(filename + str(idx),'r') as f:
@@ -43,4 +43,4 @@ def train_and_test_alloc(dataset):
         fw.write(str(alloc))
 
 if __name__ == '__main__':
-    screen_params_alloc('mnist')
+    screen_params_alloc('covtype')
--- pycode/result_show.py
+++ pycode/result_show.py
@@ -112,5 +112,5 @@ def print_test_results(dataset):
 
 if __name__ == '__main__':
     # plot_params('adult')
-    print_params('mnist')
+    print_params('covtype-adam')
     # print_test_results('covtype')
