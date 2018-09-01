95a4b7c3be033cefe5181db30abb3e4675fb2934
Yitong Sun <syitong@umich.edu>
2018-09-01 13:13:35 -0400

    revise plot_params method, plot all dataset gamma results

--- pycode/result_show.py
+++ pycode/result_show.py
@@ -1,30 +1,30 @@
 import numpy as np
 from numpy import array
 import matplotlib as mpl
-# mpl.use('Agg')
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-def plot_learning_rate():
-    tags = ['accuracy','sparsity','traintime','testtime']
-    units = ['-','-','s','s']
-    for idx in range(4):
-        tag = tags[idx]
-        unit = units[idx]
-        x_labels = np.arange(-2.,3.,0.5)
-        orfsvm = np.zeros((10,len(x_labels)))
-        urfsvm = np.zeros((10,len(x_labels)))
-        for prefix in range(1,11,1):
-            orfsvm[prefix-1,:] = np.loadtxt(
-                'result/covtype_{0:s}{2:s}{1:s}'.format(
-                'Gaussian',str(prefix), 'layer 2'))[:,idx]
-            urfsvm[prefix-1,:] = np.loadtxt(
-                'result/covtype_{0:s}{2:s}{1:s}'.format(
-                    'ReLU',str(prefix),'layer 2'))[:,idx]
-
-        orfmean = np.mean(orfsvm,axis=0)
-        urfmean = np.mean(urfsvm,axis=0)
-        orfstd = np.std(orfsvm,axis=0)
-        urfstd = np.std(urfsvm,axis=0)
+# def plot_learning_rate():
+#     tags = ['accuracy','sparsity','traintime','testtime']
+#     units = ['-','-','s','s']
+#     for idx in range(4):
+#         tag = tags[idx]
+#         unit = units[idx]
+#         x_labels = np.arange(-2.,3.,0.5)
+#         orfsvm = np.zeros((10,len(x_labels)))
+#         urfsvm = np.zeros((10,len(x_labels)))
+#         for prefix in range(1,11,1):
+#             orfsvm[prefix-1,:] = np.loadtxt(
+#                 'result/covtype_{0:s}{2:s}{1:s}'.format(
+#                 'Gaussian',str(prefix), 'layer 2'))[:,idx]
+#             urfsvm[prefix-1,:] = np.loadtxt(
+#                 'result/covtype_{0:s}{2:s}{1:s}'.format(
+#                     'ReLU',str(prefix),'layer 2'))[:,idx]
+# 
+#         orfmean = np.mean(orfsvm,axis=0)
+#         urfmean = np.mean(urfsvm,axis=0)
+#         orfstd = np.std(orfsvm,axis=0)
+#         urfstd = np.std(urfsvm,axis=0)
 
         # opt vs unif feature selection
         # plt.title("opt vs unif feature selection on MNIST")
@@ -37,34 +37,39 @@ def plot_learning_rate():
         # plt.savefig('image/opt_vs_unif.eps')
 
         # Gaussian vs ReLU random features
-        fig = plt.figure()
-        plt.title("Fourier vs ReLU Feature {} on Covtype".format(tag))
-        plt.xlabel('log opt rate (-)')
-        plt.ylabel('{0} ({1})'.format(tag,unit))
-        plt.xticks(x_labels)
-        plt.errorbar(x_labels,orfmean,yerr=orfstd,fmt='bs--',label='Fourier',fillstyle='none')
-        plt.errorbar(x_labels,urfmean,yerr=urfstd,fmt='gx:',label='ReLU')
-        plt.legend(loc=4)
-        plt.savefig('image/covtype_Fourier_vs_ReLU_{}.eps'.format(tag))
-        plt.close(fig)
+        # fig = plt.figure()
+        # plt.title("Fourier vs ReLU Feature {} on Covtype".format(tag))
+        # plt.xlabel('log opt rate (-)')
+        # plt.ylabel('{0} ({1})'.format(tag,unit))
+        # plt.xticks(x_labels)
+        # plt.errorbar(x_labels,orfmean,yerr=orfstd,fmt='bs--',label='Fourier',fillstyle='none')
+        # plt.errorbar(x_labels,urfmean,yerr=urfstd,fmt='gx:',label='ReLU')
+        # plt.legend(loc=4)
+        # plt.savefig('image/covtype_Fourier_vs_ReLU_{}.eps'.format(tag))
+        # plt.close(fig)
 
-def plot_params(dataset):
-    with open('result/'+dataset+'-alloc','r') as f:
-        result = eval(f.read())
+def _extract_xy(dataset,feature):
+    filename = 'result/{0:s}-{1:s}-screen-'.format(dataset,feature)
+    with open(filename+'alloc','r') as f:
+        result,params = eval(f.read())
     result_trim = [row[1:] for row in result[1:]]
     result_trim = np.array(result_trim)
-    F_result = np.max(result_trim[:,:-1],axis=0)
-    x_labels = result[0][1:-1]
-    R_result = [max(result_trim[:,-1])] * len(x_labels)
+    y = np.max(result_trim[:,:-1],axis=0)
+    x = result[0][1:-1]
+    return x,y
+
+def plot_params(dataset):
+    x,y1 = _extract_xy(dataset,'ReLU')
+    _,y2 = _extract_xy(dataset,'Gaussian')
     fig = plt.figure()
     plt.title("Random Features Methods on "+dataset)
     plt.xlabel('log(Gamma)')
     plt.ylabel('accuracy')
-    plt.xticks(x_labels)
+    plt.xticks(x)
     plt.ylim((0,1.01))
-    plt.plot(x_labels,F_result,'x--',label='Fourier')
-    plt.plot(x_labels,R_result,':',label='ReLU')
-    plt.legend(loc=1)
+    plt.plot(x,y1,'x--',label='ReLU')
+    plt.plot(x,y2,'o:',label='Fourier')
+    plt.legend(loc=3)
     plt.savefig('image/{}-gamma.eps'.format(dataset))
     plt.close(fig)
 
@@ -115,4 +120,11 @@ def print_test_results(dataset,feature):
     _dict_print(fit_params)
 
 if __name__ == '__main__':
-    print_test_results('adult','Gaussian')
+    plot_params('covtype')
+    plot_params('mnist')
+    plot_params('adult')
+    plot_params('checkboard')
+    plot_params('square')
+    plot_params('strips')
+    plot_params('sine1')
+    plot_params('sine1-10')
