eea4581e9d330c22b9bb1291b6a7d061ff1565df
Yitong Sun <syitong@umich.edu>
2018-09-01 20:29:32 -0400

    print results to tex table format

---
+++ pycode/print2tex.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy import array
+
+fnames = ['sine1','sine1-10','strips',
+        'square','checkboard','adult',
+        'mnist','covtype']
+tags = ['accuracy','traintime','testtime']
+features = ['Gaussian','ReLU']
+
+if __name__ == '__main__':
+    fw = open('alloc4tex','w')
+    for tag in tags:
+        fw.write(' & '+tag)
+    for tag in tags:
+        fw.write(' & '+tag)
+    fw.write(' \\\\')
+    fw.write('\n')
+    for fname in fnames:
+        feature = 'Gaussian'
+        with open('result/'+fname+'-'+feature+'-test-alloc','r') as f:
+            result1 = eval(f.read())[0]
+        feature = 'ReLU'
+        with open('result/'+fname+'-'+feature+'-test-alloc','r') as f:
+            result2 = eval(f.read())[0]
+        fw.write(fname)
+        for tag in tags:
+            fw.write(' & {mean:.3f}({std:.3f})'.format(**result1[tag]))
+            fw.write(' & {mean:.3f}({std:.3f})'.format(**result2[tag]))
+        fw.write(' \\\\')
+        fw.write('\n')
+    fw.close()
+
